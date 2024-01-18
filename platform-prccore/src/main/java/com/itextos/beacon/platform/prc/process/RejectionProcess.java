c2dbcbe428263ec77300f654881b2d0ddb207a30
Jaffer Sadhik <jaffer@technowizards.co.in>
2024-01-18 21:06:53 +0530

    jj

--- platform-prccore/src/main/java/com/itextos/beacon/platform/prc/process/RejectionProcess.java
+++ platform-prccore/src/main/java/com/itextos/beacon/platform/prc/process/RejectionProcess.java
@@ -66,8 +66,8 @@ public class RejectionProcess
             boolean       canProcessMultiple = false;
             final boolean lprocessDNCarrier  = PRCUtil.processDNDToCarrier(aMessageRequest);
 
-            aMessageRequest.setBaseSmsRate(0);
-            aMessageRequest.setBaseAddFixedRate(0);
+            aMessageRequest.setBillingAddFixedRate(0);
+            aMessageRequest.setBillingSmsRate(0);
             if (log.isDebugEnabled())
                 log.debug("Is Process DN Carrier for DND Fail :" + lprocessDNCarrier);
 
