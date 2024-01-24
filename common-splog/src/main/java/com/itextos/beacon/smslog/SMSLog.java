54b6f80b91ef65205f0790c9f2882c0d9fa9b6cb
Jaffer Sadhik <jaffer@technowizards.co.in>
2024-01-24 20:06:49 +0530

    jj

--- common-splog/src/main/java/com/itextos/beacon/smslog/SMSLog.java
+++ common-splog/src/main/java/com/itextos/beacon/smslog/SMSLog.java
@@ -1,20 +1,54 @@
 package com.itextos.beacon.smslog;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 
 public class SMSLog {
 
-    private static final Log                                                log                           = LogFactory.getLog(SMSLog.class);
+
+    private static final  Logger logger = Logger.getLogger(SMSLog.class.getName());
+    
+    static {
+    	
+
+        String logFileNamePattern = "/logs/smslog.%g.log";
+
+        // Create a FileHandler with the specified log file name
+        FileHandler fileHandler=null;
+		try {
+			fileHandler = new FileHandler(logFileNamePattern, 1000000, 10, true);
+			
+			   // Set the logging level for the handler
+	        fileHandler.setLevel(Level.INFO);
+
+	        // Set a formatter for the handler (optional)
+	        fileHandler.setFormatter(new SimpleFormatter());
+
+	        // Add the handler to the logger
+	        logger.addHandler(fileHandler);
+	        
+	        logger.setLevel(Level.INFO);
+
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+     
+
+        // Set the logging level for the logger
+    }
 
     public static void log(String string) {
-    	
-    	if(log.isDebugEnabled()) {
-    		
-        	log.debug(string);
 
-    	}
+    	logger.info(string);
     	
     }
 }
