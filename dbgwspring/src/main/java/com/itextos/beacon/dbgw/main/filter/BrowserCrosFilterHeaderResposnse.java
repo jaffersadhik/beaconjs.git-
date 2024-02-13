2c44de0a6567f46d5823f3a8f087dbf26b899a68
Jaffer Sadhik <jaffer@technowizards.co.in>
2024-02-13 11:12:57 +0530

    jj

--- dbgwspring/src/main/java/com/itextos/beacon/dbgw/main/filter/BrowserCrosFilterHeaderResposnse.java
+++ dbgwspring/src/main/java/com/itextos/beacon/dbgw/main/filter/BrowserCrosFilterHeaderResposnse.java
@@ -1,21 +1,23 @@
 package com.itextos.beacon.dbgw.main.filter;
 
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
+import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsFilter implements Filter {
+public class BrowserCrosFilterHeaderResposnse implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
