9a28f597ee37ede5dba76787ec2ccb082ffc7160
Jaffer Sadhik <jaffer@technowizards.co.in>
2024-02-13 11:03:25 +0530

    jj

--- dbgwspring/src/main/java/com/itextos/beacon/dbgw/main/filter/CorsFilter.java
+++ dbgwspring/src/main/java/com/itextos/beacon/dbgw/main/filter/CorsFilter.java
@@ -21,7 +21,7 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
-        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
         httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
