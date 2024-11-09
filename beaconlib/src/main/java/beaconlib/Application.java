97300e6d3035e39b70acfa0bfa17527856917cd0
Jaffer Sadhik <jaffer@technowizards.co.in>
2024-11-09 17:28:27 +0530

    'jj'

---
+++ beaconlib/src/main/java/beaconlib/Application.java
@@ -0,0 +1,17 @@
+package beaconlib;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		try {
+		com.itextos.beacon.App.main(args);
+		}catch(Exception e) {
+			
+			e.printStackTrace();
+		}
+			
+		}
+	}
+
+
