72bd31e68d02687aceb0fc8b75fd1dc9d7e7b319
Jaffer Sadhik <jaffer@technowizards.co.in>
2024-11-09 06:55:09 +0530

    'jj'

--- common-pwdencryption/src/main/java/com/itextos/beacon/commonlib/pwdencryption/Encryptor.java
+++ common-pwdencryption/src/main/java/com/itextos/beacon/commonlib/pwdencryption/Encryptor.java
@@ -155,17 +155,17 @@ public class Encryptor
         	}*/
         	
       	  final String lDecryptedDbPassword = getSmppDecryptedPassword("hs+ItzXLENtN1DfrvK1teWxlYjFhdWpCYkRtdo3iEJ+IRekwl8/G7fn3");
-          System.out.println(lDecryptedDbPassword);
+          System.out.println(lDecryptedDbPassword);//Pf5X7wpt
           
         	
         	 final String lApiPassword = getApiDecryptedPassword("i1GTPzBhdrTTErUi2F6U629SVVNLNWF3dFZYTXn/pv6C3V77YZLFNXYY");
-             System.out.println(lApiPassword);
+             System.out.println(lApiPassword);//WPzDMqbaVqG8
         	
         	
 
-        //	final String lDecryptedDbPassword = getDecryptedDbPassword("N5mIleJjtYx2EFg8+cd3uFpGaUgxdEpKQjde+JBw9AjmsAX7iQEVAvlI");
+        	final String lDbPassword = getDecryptedDbPassword("N5mIleJjtYx2EFg8+cd3uFpGaUgxdEpKQjde+JBw9AjmsAX7iQEVAvlI");
 
-      //      System.out.println(lDecryptedDbPassword);
+           System.out.println(lDbPassword);//Sy5Cf8@123
 
        
            
