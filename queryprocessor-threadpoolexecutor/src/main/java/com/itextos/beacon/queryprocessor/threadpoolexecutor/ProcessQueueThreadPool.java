b3c8ffde66d1ffddb74ea151025e7c6c7aae4f6d
Jaffer Sadhik <jaffer@technowizards.co.in>
2024-12-25 11:40:25 +0530

    'jj'

--- queryprocessor-threadpoolexecutor/src/main/java/com/itextos/beacon/queryprocessor/threadpoolexecutor/ProcessQueueThreadPool.java
+++ queryprocessor-threadpoolexecutor/src/main/java/com/itextos/beacon/queryprocessor/threadpoolexecutor/ProcessQueueThreadPool.java
@@ -52,12 +52,14 @@ public class ProcessQueueThreadPool
 
         try
         {
+        	/*
             final String cfg_fn ="/task_executor.properties";// args[0];
             log.info("Reading values from config file: " + cfg_fn);
             final FileReader file = new FileReader(args[0]);
             mySQL_cfg_val.load(file);
             file.close();
-            final ConnectionPoolSingleton connPool = ConnectionPoolSingleton.createInstance();
+            */
+        	final ConnectionPoolSingleton connPool = ConnectionPoolSingleton.createInstance();
             masterDBConn = DBConnectionProvider.getMasterDBConnection();
 
             MAX_T        = Utility.getInteger(mySQL_cfg_val.getProperty("maxExecutorThreads").trim());
