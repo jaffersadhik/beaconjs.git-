f9b8a37dee1fbb58b57807dd5dfd3d65047d4804
Jaffer Sadhik <jaffer@technowizards.co.in>
2024-12-30 21:10:11 +0530

    'jj'

--- mainmodule/src/main/java/com/itextos/beacon/App.java
+++ mainmodule/src/main/java/com/itextos/beacon/App.java
@@ -171,6 +171,14 @@ public class App {
 
 				return true;
 				
+			}else if(module.equals("passwordapi")){
+				
+				com.itextos.beacon.jettyserver.passwordencryption.QueryEngine.main(args);
+				
+				IS_START_PROMETHEUS=true;
+
+				return true;
+				
 			}
 			
 			return false;
